675266cc2a9f2a7df0dc059dc042fbe62e31d637
gregbodet <37414914+gregbodet@users.noreply.github.com>
2018-10-05 15:01:07 +0200

    Update init_bd_planinteractif.sql

--- sql/init_bd_planinteractif.sql
+++ sql/init_bd_planinteractif.sql
@@ -2350,7 +2350,77 @@ GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_tic_la_tampon TO sig_create;
 GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_tic_la_tampon TO create_sig;
 GRANT SELECT ON TABLE x_apps_public.xappspublic_geo_v_tic_la_tampon TO read_sig;
 
+-- ************************************************************************************************************************
+-- *** TRI, DECHET
+-- ************************************************************************************************************************
 
+-- View: x_apps_public.xappspublic_geo_v_dec_pav_tampon
+
+-- DROP VIEW x_apps_public.xappspublic_geo_v_dec_pav_tampon;
+
+CREATE OR REPLACE VIEW x_apps_public.xappspublic_geo_v_dec_pav_tampon AS
+ SELECT geo_dec_pav_verre.id_contver,
+    geo_dec_pav_verre.v_tampon,
+    geo_dec_pav_verre.date_effet,
+    geo_dec_pav_verre.geom2
+   FROM m_dechet.geo_dec_pav_verre
+  WHERE geo_dec_pav_verre.statut::text = '10'::text AND geo_dec_pav_verre.env_implan::text <> '40'::text;
+
+ALTER TABLE x_apps_public.xappspublic_geo_v_dec_pav_tampon
+    OWNER TO sig_create;
+COMMENT ON VIEW x_apps_public.xappspublic_geo_v_dec_pav_tampon
+    IS 'Vue géométrique contenant les tampons d''emprise des conteneurs TLC pour EXPORT FME et recherche des adresse dans ses tampons pour remonter le PAV VERRE';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tampon TO edit_sig;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tampon TO sig_create;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tampon TO create_sig;
+GRANT SELECT ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tampon TO read_sig;
+
+-- View: x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon
+
+-- DROP VIEW x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon;
+
+CREATE OR REPLACE VIEW x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon AS
+ SELECT geo_dec_pav_tlc.id_cont_tl,
+    geo_dec_pav_tlc.v_tampon,
+    geo_dec_pav_tlc.date_effet,
+    geo_dec_pav_tlc.geom2
+   FROM m_dechet.geo_dec_pav_tlc;
+
+ALTER TABLE x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon
+    OWNER TO sig_create;
+COMMENT ON VIEW x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon
+    IS 'Vue géométrique contenant les tampons d''emprise des conteneurs TLC pour EXPORT FME et recherche des adresse dans ses tampons pour remonter le PAV VERRE';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon TO edit_sig;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon TO sig_create;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon TO create_sig;
+GRANT SELECT ON TABLE x_apps_public.xappspublic_geo_v_dec_pav_tlc_tampon TO read_sig;
+
+-- View: x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur
+
+-- DROP VIEW x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur;
+
+CREATE OR REPLACE VIEW x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur AS
+ SELECT geo_dec_secteur_enc.gid,
+    geo_dec_secteur_enc.insee,
+    geo_dec_secteur_enc.commune,
+    geo_dec_secteur_enc.adresse,
+    geo_dec_secteur_enc.l_secteur,
+    geo_dec_secteur_enc.l_message1,
+    geo_dec_secteur_enc.l_message2,
+    geo_dec_secteur_enc.geom1 AS geom
+   FROM m_dechet.geo_dec_secteur_enc;
+
+ALTER TABLE x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur
+    OWNER TO sig_create;
+COMMENT ON VIEW x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur
+    IS 'Vue géométrique contenant les secteurs de rammassage des encombrants pour export dans GEO APPLI GD PUBLIC';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur TO edit_sig;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur TO sig_create;
+GRANT ALL ON TABLE x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur TO create_sig;
+GRANT SELECT ON TABLE x_apps_public.xappspublic_geo_v_dec_secteur_enc_secteur TO read_sig;
 
 
 
